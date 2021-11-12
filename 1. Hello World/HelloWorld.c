ca05519bfdcea27c18c27fd9d64e2df2e684a6a3
Anik Ahmed <anikahmed0091@gmail.com>
2021-11-12 12:28:37 +0600

    🎈 add HelloWorld.c

---
+++ 1. Hello World/HelloWorld.c
@@ -0,0 +1,19 @@
+// Hello World!
+// From Class 1.
+// A simple program to print some text.
+
+// stdio -> standard input output.
+#include <stdio.h>
+
+// int as the function returns integer value.
+int main() {
+
+	// printf() is used to print anythiing!
+	printf("Hello World");
+
+	// \n to start new line.
+	printf("\nMy name is Anik");
+
+	// If the code output returns 0, it means the code is bug free.
+	return 0;
+}

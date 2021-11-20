6d16010bf75f498d4b63dcf1281dff6dcfb6e8d9
Anik Ahmed <anikahmed0091@gmail.com>
2021-11-20 21:42:37 +0600

    🐯 add practice problem 4

---
+++ 0. Practice Problems/4_triangleAreaThreeSides.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float side1, side2, side3, s, t, area;
+
+	printf("Enter three sides of the triangle with space: \n");
+	scanf("%f %f %f", &side1, &side2, &side3);
+
+	s = (side1 + side2 + side3)/2;
+	t = s*(s - side1)*(s - side2)*(s - side3);
+	area = sqrt(t);
+
+	printf("Area of the triangle is: \n %f", area);
+
+	return 0;
+}

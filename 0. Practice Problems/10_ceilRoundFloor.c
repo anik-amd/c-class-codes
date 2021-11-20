b39f4a5b2ff49170a40a2f79d3a5f230b79b3c2f
Anik Ahmed <anikahmed0091@gmail.com>
2021-11-21 00:26:24 +0600

    🐭 add practice problem 10

---
+++ 0. Practice Problems/10_ceilRoundFloor.c
@@ -0,0 +1,24 @@
+// Practice problem 10.
+// Takes a floating-point number & 
+// outputs ceil, round & floor of the number.
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float a;
+	int ce, ro, fl;
+
+	printf("Enter a floating-point number: ");
+	scanf("%f", &a);
+
+	ce = ceil(a);
+	ro = round(a);
+	fl = floor(a);
+
+	printf("Ceil of %f: %d \n", a, ce);
+	printf("Round of %f: %d \n", a, ro);
+	printf("Floor of %f: %d \n", a, fl);
+
+	return 0;
+} 

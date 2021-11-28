95b11d231ca76c4978f4d8b8c083f79b92f2a32c
Anik Ahmed <anikahmed0091@gmail.com>
2021-11-29 00:01:12 +0600

    ⛏ remove unnecessary lines

--- 0. Practice Problems/6_circleArea.c
+++ 0. Practice Problems/6_circleArea.c
@@ -2,12 +2,11 @@
 // Takes radius of a circle as input & prints area of the circle.
 
 #include <stdio.h>
-#include <conio.h>
 
 #define PI 3.141
 
 int main() {
-	float radius, area, pi;
+	float radius, area;
 
 	printf("Enter radius of the circle: ");
 	scanf("%f", &radius);

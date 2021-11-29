f7f97a19b54cf6816b21f863c079647d4be0fade
Anik Ahmed <anikahmed0091@gmail.com>
2021-11-29 16:33:06 +0600

    🎈 add problem 9

---
+++ 3. Expressions & Operators/swap.c
@@ -0,0 +1,22 @@
+// Takes two numbers, stores in a & b variable &
+// swaps value of a & b variable.
+
+#include <stdio.h>
+
+int main() {
+	int a,b,temp;
+
+	printf("Enter value of first number (a): ");
+	scanf("%d", &a);
+
+	printf("Enter value of second number (b): ");
+	scanf("%d", &b);
+
+	temp = a;
+	a = b;
+	b = temp;
+
+	printf("After swapping, value of a is %d & value of b is %d. \n", a ,b);
+
+	return 0;
+}

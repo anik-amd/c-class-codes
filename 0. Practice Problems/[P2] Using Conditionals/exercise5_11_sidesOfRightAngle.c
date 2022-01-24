a50eb5d44f6a21915981726cb680653c71a5b987
Anik Ahmed <anikahmed0091@gmail.com>
2022-01-24 23:37:58 +0600

    ✨ feat: add new practice problem

---
+++ 0. Practice Problems/[P2] Using Conditionals/exercise5_11_sidesOfRightAngle.c
@@ -0,0 +1,20 @@
+// Takes theree sides of an triangle & checks if it's a right angle.
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter values of three sides: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b)
+    {
+        printf("It is a right angled triangle.");
+    }
+    else
+    {
+        printf("It is not a right angled triangle.");
+    }
+}

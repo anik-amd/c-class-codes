01c4783dd6ea9c2ea6cf54e656f89823f866f960
Anik Ahmed <anikahmed0091@gmail.com>
2022-01-24 20:50:48 +0600

    ✨ feat: add new practice problem

---
+++ 0. Practice Problems/[P2] Using Conditionals/9_smallCapitalLetter.c
@@ -0,0 +1,27 @@
+// Check whether a character is a capital letter or a small letter or others.
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a letter: ");
+    scanf("%c", &ch);
+
+    // Note that single quote (' ') denotes single character & double quote denotes string.
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        printf("%c is a capital letter.", ch);
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        printf("%c is a small letter.", ch);
+    }
+    else
+    {
+        printf("%c is not a letter.", ch);
+    }
+
+    return 0;
+}

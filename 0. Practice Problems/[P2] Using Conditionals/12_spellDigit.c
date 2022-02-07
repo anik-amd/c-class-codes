92d629271de122377049094febfc4a922e38d75b
Anik Ahmed <anikahmed0091@gmail.com>
2022-02-07 19:43:34 +0600

    ♻️ refactor: brackets

--- 0. Practice Problems/[P2] Using Conditionals/12_spellDigit.c
+++ 0. Practice Problems/[P2] Using Conditionals/12_spellDigit.c
@@ -2,15 +2,13 @@
 
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int digit;
 
     printf("Enter a digit: ");
     scanf("%d", &digit);
 
-    switch (digit)
-    {
+    switch (digit) {
     case 0:
         printf("Zero.");
         break;

feee9101be3b98a27dd52d57b2122f13330c0973
Anik Ahmed <anikahmed0091@gmail.com>
2022-04-10 23:52:14 +0600

    🎨 style: indentations

--- 0. Practice Problems/[P3] Loops/2j.c
+++ 0. Practice Problems/[P3] Loops/2j.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int i, sum = 0;
+  int i, sum = 0;
 
-    for(i = 60; i >= 0; i = i - 3) {
-        sum = sum + i;
-        // printf("%d + ", i);
-    }
+  for (i = 60; i >= 0; i = i - 3) {
+    sum = sum + i;
+    // printf("%d + ", i);
+  }
 
-    printf("\n %d", sum);
+  printf("\n %d", sum);
 
-    return 0;
+  return 0;
 }

ff6f2ec5d30cdf1c7c8437256d4443dc7e8b5e3d
Anik Ahmed <anikahmed0091@gmail.com>
2022-04-11 15:59:54 +0600

    ✨ feat: add analysis on reverse number

--- 0. Practice Problems/[P3] Loops/10.c
+++ 0. Practice Problems/[P3] Loops/10.c
@@ -1,18 +1,21 @@
+// 123 -> 321
+// 1*100+2*10+3*1 -> 3*100+2*10+1*1
+
 #include <stdio.h>
 
 int main() {
-    int n, last_digit, rev = 0;
+  int n, last_digit, rev = 0;
 
-    printf("Enter value of N: ");
-    scanf("%d", &n);
+  printf("Enter value of N: ");
+  scanf("%d", &n);
 
-    while(n != 0) {
-        last_digit = n % 10;
-        rev = rev * 10 + last_digit;
-        n = n / 10;
-    }
+  while (n != 0) {
+    last_digit = n % 10;
+    rev = rev * 10 + last_digit;
+    n = n / 10;
+  }
 
-    printf("%d", rev);
+  printf("%d", rev);
 
-    return 0;
+  return 0;
 }
